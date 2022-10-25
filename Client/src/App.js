9810ac1cd66c0987334a6c73a98423574ad04e05
korean-davinci <wo55ow231@gmail.com>
2022-10-25 16:40:31 +0900

    folder structure create

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,7 +1,25 @@
 import './App.css';
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
+import styled from "styled-components";
+import axios from "axios";
+import { Reset } from 'styled-reset';
+
 
 function App() {
-  return <div className="App"> testsetest</div>;
+
+  return (
+    <>
+      <Reset/>
+      <GlobalStyles />
+      <Router>
+        <UserContext.Provider value={{user, checkAuth, editUser}}>
+          <Header />
+        </UserContext.Provider>
+      </Router>
+    </>
+
+  );
 }
 
 export default App;
