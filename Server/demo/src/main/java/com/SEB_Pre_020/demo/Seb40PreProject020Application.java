fd092118a5aa325f199fe408d9149cf5f21a1a9a
kjh42447 <kjh42447@naver.com>
2022-10-25 16:56:31 +0900

    init audit

--- Server/demo/src/main/java/com/SEB_Pre_020/demo/Seb40PreProject020Application.java
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/Seb40PreProject020Application.java
@@ -2,7 +2,9 @@ package com.SEB_Pre_020.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Seb40PreProject020Application {
 
