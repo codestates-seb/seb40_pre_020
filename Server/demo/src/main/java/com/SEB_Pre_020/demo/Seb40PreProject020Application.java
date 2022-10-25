6a6551527f25a48a6a71bd919481d612e69c6ff1
SJUANS <lovejuan95@hanmail.net>
2022-10-25 15:25:15 +0900

    Java 백엔드 Spring boot 템플릿 파일

---
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/Seb40PreProject020Application.java
@@ -0,0 +1,13 @@
+package com.SEB_Pre_020.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seb40PreProject020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seb40PreProject020Application.class, args);
+	}
+
+}
---
+++ Server/demo/src/test/java/com/SEB_Pre_020/demo/Seb40PreProject020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SEB_Pre_020.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seb40PreProject020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
