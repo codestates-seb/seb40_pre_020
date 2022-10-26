e3be218dd45835ae5d6544463cbf33336396a050
kjh42447 <kjh42447@naver.com>
2022-10-27 00:23:12 +0900

    post complete(not implement member ver)

--- Server/demo/src/main/java/com/SEB_Pre_020/demo/member/entity/Member.java
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/member/entity/Member.java
@@ -4,7 +4,6 @@ import com.SEB_Pre_020.demo.audit.Auditable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.*;
 
