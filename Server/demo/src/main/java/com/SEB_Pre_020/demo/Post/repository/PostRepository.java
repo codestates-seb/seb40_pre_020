d89d96d9300a785546b3619f675a79b7bbb33c8c
kjh42447 <kjh42447@naver.com>
2022-10-26 23:42:04 +0900

    post

--- Server/demo/src/main/java/com/SEB_Pre_020/demo/Post/repository/PostRepository.java
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/Post/repository/PostRepository.java
@@ -3,7 +3,9 @@ package com.SEB_Pre_020.demo.Post.repository;
 import com.SEB_Pre_020.demo.Post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    List<Post> findByParentId(int parentId);
 }
--- Server/demo/src/main/java/com/SEB_Pre_020/demo/Post/service/PostService.java
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/Post/service/PostService.java
@@ -2,10 +2,15 @@ package com.SEB_Pre_020.demo.Post.service;
 
 import com.SEB_Pre_020.demo.Post.entity.Post;
 import com.SEB_Pre_020.demo.Post.repository.PostRepository;
+import org.springframework.beans.support.PagedListHolder;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Transactional
@@ -40,17 +45,38 @@ public class PostService {
         return postRepository.save(findPost);
     }
 
-    // 특정 게시글(답글) 하나만 get
+    /** 특정 게시글(답글) 하나만 get */
     @Transactional(readOnly = true)
     public Post findPost(int postId) {
         return findVerifiedPost(postId);
     }
 
-    // 게시글의 모든 답변
-//    @Transactional
-//    public Page<Post> findPostPosts(int postId, int page, int size) {
-//
-//    }
+    /** 게시글의 모든 답변 get */
+    @Transactional
+    public Page<Post> findPostPosts(int postId, int page, int size) {
+        List<Post> postList = postRepository.findByParentId(postId);
+
+        Pageable pageable = PageRequest.of(page, size);
+        PagedListHolder pagedListHolder = new PagedListHolder(postList);
+        pagedListHolder.setPageSize(size);
+        pagedListHolder.setPage(page);
+
+        return new PageImpl<>(pagedListHolder.getPageList(), pageable, postList.size());
+    }
+
+    // 멤버 get 현재 미구현
+    @Transactional
+    public List<Post> findMemberPosts(int postId, int page, int size) {
+        List<Post> postList = postRepository.findByParentId(postId);
+        return postList;
+    }
+
+    @Transactional
+    public void deletePost(int postId) {
+        Post findPost = findVerifiedPost(postId);
+
+        postRepository.delete(findPost);
+    }
 
     @Transactional(readOnly = true)
     public Post findVerifiedPost(int postId) {
@@ -58,5 +84,6 @@ public class PostService {
         Post findPost = optionalPost.orElseThrow();     // Exception code 추가 필요
         return findPost;
     }
+
 
 }
