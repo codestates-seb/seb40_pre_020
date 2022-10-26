8cadfe1ca5224d58e4e88f972e564de0bb0b36da
SJUANS <lovejuan95@hanmail.net>
2022-10-26 15:42:39 +0900

    MemberService: 클래스 기본 틀 작성

---
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/member/service/MemberService.java
@@ -0,0 +1,10 @@
+package com.SEB_Pre_020.demo.member.service;
+
+import com.SEB_Pre_020.demo.member.entity.Member;
+
+public class MemberService {
+    public Member createMember(Member member) {
+        Member createdMember = member;
+        return createdMember;
+    } // 회원등록 메서드 작성
+}
