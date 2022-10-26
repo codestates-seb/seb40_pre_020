7ff34b7d9fa4a8e585ba2e2929efd5eb66b71343
SJUANS <lovejuan95@hanmail.net>
2022-10-26 14:18:37 +0900

    Member dto 클래스 생성

---
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/member/dto/MemberDto.java
@@ -0,0 +1,8 @@
+package com.SEB_Pre_020.demo.member.dto;
+
+import javax.validation.constraints.Email;
+
+public class MemberDto {
+//    @Email
+//    private String email; //핸들러 클래스가 완성된 다음 DTO 필요한 부분 작성
+}
