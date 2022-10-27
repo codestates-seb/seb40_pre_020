e6012787c4fbc3c2cc011f93d6a248efa91b02f5
SJUANS <lovejuan95@hanmail.net>
2022-10-27 14:49:50 +0900

    MemberService, MemberController, MemberMapper 클래스 작성 (현재 회원등록 기능까지 구현)

--- Server/demo/src/main/java/com/SEB_Pre_020/demo/member/controller/MemberController.java
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/member/controller/MemberController.java
@@ -1,9 +1,15 @@
 package com.SEB_Pre_020.demo.member.controller;
 
+import com.SEB_Pre_020.demo.dto.SingleResponseDto;
+import com.SEB_Pre_020.demo.member.dto.MemberPostDto;
 import com.SEB_Pre_020.demo.member.entity.Member;
+import com.SEB_Pre_020.demo.member.mapstruct.mapper.MemberMapper;
 import com.SEB_Pre_020.demo.member.repository.MemberRepository;
+import com.SEB_Pre_020.demo.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -12,23 +18,35 @@ import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value="v1/members")
+@RequestMapping(value="/v1")
 public class MemberController {
-    private final MemberRepository memberRepository;
 
-    @GetMapping("")
-    public String addForm(@ModelAttribute("member") Member member){
-        return "수정";
+    private MemberService memberService;
+
+    private MemberMapper memberMapper;
+
+    public MemberController(MemberService memberService, MemberMapper memberMapper) {
+        this.memberService = memberService;
+        this.memberMapper = memberMapper;
     }
 
-    @PostMapping("수정")
-    public String save(@Valid @ModelAttribute Member member, BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
-            return "수정";
-        }
-
-        memberRepository.save(member);
-        return "redirect:/";
+    @PostMapping("/signup") // 회원가입
+    public ResponseEntity postMember(@Valid @RequestBody MemberPostDto memberPostDto){
+       Member member = memberMapper.memberDtoToMember(memberPostDto);
+       Member joinedMember = memberService.joinMember(member);
+       return new ResponseEntity<>(
+               new SingleResponseDto<>(memberMapper.memberToMemberResponseDto(joinedMember)),
+               HttpStatus.CREATED);
     }
-
+/** jwt 토큰 적용 뒤 완성해야 할 코드
+//    @GetMapping("/member") // 로그인한 회원 정보 토큰 클라이언트에게 전달
+//    public ResponseEntity getMember(){
+//        Member member = memberService.getLoginMember();
+//
+//        return new ResponseEntity<>(
+//                new SingleResponseDto<>(memberMapper.memberToMemberResponseDto(member)),
+//                HttpStatus.OK);
+//    }
+//    @PutMapping("/profile/memberid/{MemberId}") // 마이페이지 프로필 수정
+ **/
 }
--- Server/demo/src/main/java/com/SEB_Pre_020/demo/member/mapstruct/mapper/MemberMapper.java
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/member/mapstruct/mapper/MemberMapper.java
@@ -1,12 +1,28 @@
 package com.SEB_Pre_020.demo.member.mapstruct.mapper;
 
 import com.SEB_Pre_020.demo.member.dto.MemberPostDto;
+import com.SEB_Pre_020.demo.member.dto.MemberResponseDto;
 import com.SEB_Pre_020.demo.member.entity.Member;
 import org.mapstruct.Mapper; // MapSturuct 매퍼 인터페이스로 정의하는 애너테이션
 //매퍼 인터페이스를 정의하면 MapStruct가 자동으로 매퍼 구현 클래스를 생성해줌
 
 @Mapper(componentModel = "Spring") // Spring Bean으로 등록하는 애트리뷰트
 public interface MemberMapper {
-    Member memberDtoToMember(MemberPostDto memberPostDto);
-    MemberResponseDto memberToMemberResponseDto(Member member);
+    default Member memberDtoToMember(MemberPostDto memberPostDto){
+      Member member = new Member();
+
+      member.setEmail(memberPostDto.getEmail());
+      member.setName(memberPostDto.getName());
+      member.setPassword(memberPostDto.getPassword());
+
+      return member;
+    }
+    default MemberResponseDto memberToMemberResponseDto(Member member){
+      MemberResponseDto memberResponseDto = new MemberResponseDto();
+
+      memberResponseDto.setEmail(member.getEmail());
+      memberResponseDto.setName(member.getName()); // 프로필에 표시되는 회원정보
+
+      return memberResponseDto;
+    }
 }
