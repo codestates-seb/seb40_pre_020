c0f67c5b2e7f96714937d3186fffa4c9d27f1506
kjh42447 <kjh42447@naver.com>
2022-10-27 20:05:13 +0900

    api documentation postPatch

--- Server/demo/src/test/java/com/SEB_Pre_020/demo/post/PostApiTests.java
+++ Server/demo/src/test/java/com/SEB_Pre_020/demo/post/PostApiTests.java
@@ -18,6 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -27,7 +28,7 @@ import java.util.List;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
@@ -89,6 +90,63 @@ public class PostApiTests {
                                         fieldWithPath("postContent").type(JsonFieldType.STRING).description("내용"),
                                         fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("작성자 Id"),
                                         fieldWithPath("postView").type(JsonFieldType.NUMBER).description("조회수")
+                                )
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 데이터"),
+                                        fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("게시글 Id"),
+                                        fieldWithPath("data.parentId").type(JsonFieldType.NUMBER).description("부모 Id"),
+                                        fieldWithPath("data.postTitle").type(JsonFieldType.STRING).description("제목"),
+                                        fieldWithPath("data.postContent").type(JsonFieldType.STRING).description("내용"),
+                                        fieldWithPath("data.memberId").type(JsonFieldType.NUMBER).description("작성자 Id"),
+                                        fieldWithPath("data.postView").type(JsonFieldType.NUMBER).description("조회수")
+                                )
+                        )
+                ));
+    }
+
+    @Test
+    public void postPatchTest() throws Exception {
+        // given
+        int id = 1;
+        PostDto.Patch patchDto = new PostDto.Patch(1, 0, "Post1", "Content1", 1, 0);
+        PostDto.Response responseDto = new PostDto.Response(1, 0, "Post1", "Content1", 1, 0);
+        String content = gson.toJson(patchDto);
+
+        given(postMapper.postPatchToPost(Mockito.any(PostDto.Patch.class))).willReturn(new Post());
+        given(postService.updatePost(Mockito.any(Post.class))).willReturn(new Post());
+        given(postMapper.postToPostResponse(Mockito.any(Post.class))).willReturn(responseDto);
+
+        //when
+        ResultActions actions = mockMvc.perform(
+                RestDocumentationRequestBuilders.patch("/posts/{PostId}", id)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+        );
+
+        //then
+        actions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.postTitle").value(patchDto.getPostTitle()))
+                .andExpect(jsonPath("$.data.memberId").value(patchDto.getMemberId()))
+                .andExpect(jsonPath("$.data.postContent").value(patchDto.getPostContent()))
+                .andDo(document(
+                        "post-patch",
+                        ApiDocumentUtils.getRequestPreProcessor(),
+                        ApiDocumentUtils.getResponsePreProcessor(),
+                        pathParameters(
+                                parameterWithName("PostId").description("게시글 Id")
+                        ),
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("id").type(JsonFieldType.NUMBER).description("게시글 Id").ignored(),
+                                        fieldWithPath("parentId").type(JsonFieldType.NUMBER).description("부모 Id").ignored(),
+                                        fieldWithPath("postTitle").type(JsonFieldType.STRING).description("제목").optional(),
+                                        fieldWithPath("postContent").type(JsonFieldType.STRING).description("내용").optional(),
+                                        fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("작성자 Id").ignored(),
+                                        fieldWithPath("postView").type(JsonFieldType.NUMBER).description("조회수").optional()
                                 )
                         ),
                         responseFields(
