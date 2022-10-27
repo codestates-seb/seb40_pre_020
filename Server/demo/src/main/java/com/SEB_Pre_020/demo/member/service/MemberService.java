425615baf105e16d49e709ffeb6f0888faa2424f
SJUANS <lovejuan95@hanmail.net>
2022-10-27 12:34:02 +0900

    Exception 패키지(ExceptionCode, BusinessLogicException 클래스)추가,이미 존재하는 이메일로 회원가입할 경우 예외처리 코드 MemberService에 추가

--- Server/demo/src/main/java/com/SEB_Pre_020/demo/member/service/MemberService.java
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/member/service/MemberService.java
@@ -1,10 +1,27 @@
 package com.SEB_Pre_020.demo.member.service;
 
+import com.SEB_Pre_020.demo.exception.ExceptionCode;
 import com.SEB_Pre_020.demo.member.entity.Member;
+import com.SEB_Pre_020.demo.member.repository.MemberRepository;
+import com.SEB_Pre_020.demo.exception.BusinessLogicException;
+
+import java.util.Optional;
 
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
     public Member createMember(Member member) {
-        Member createdMember = member;
-        return createdMember;
-    } // 회원등록 메서드 작성
+        verifyExistsEmail(member.getEmail());
+        return memberRepository.save(member);
+    } // 회원등록 메서드
+
+    private void verifyExistsEmail(String email) {
+        Optional<Member> member = MemberRepository.findByEmail(email);
+        if (member.isPresent())
+            throw new BusinessLogicException(ExceptionCode.USER_EXISTS);
+    }
 }
