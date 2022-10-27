2e4148ac608088bbe27e2070c1d1f3f4d4a0012e
kjh42447 <kjh42447@naver.com>
2022-10-27 18:05:40 +0900

    api documentation postPost

--- Server/demo/src/test/java/com/SEB_Pre_020/demo/post/PostApiTests.java
+++ Server/demo/src/test/java/com/SEB_Pre_020/demo/post/PostApiTests.java
@@ -1,23 +1,107 @@
 package com.SEB_Pre_020.demo.post;
 
+import com.SEB_Pre_020.demo.Post.controller.PostController;
+import com.SEB_Pre_020.demo.Post.dto.PostDto;
+import com.SEB_Pre_020.demo.Post.entity.Post;
+import com.SEB_Pre_020.demo.Post.mapper.PostMapper;
+import com.SEB_Pre_020.demo.Post.service.PostService;
+import com.SEB_Pre_020.demo.member.controller.MemberController;
+import com.SEB_Pre_020.demo.util.ApiDocumentUtils;
 import org.junit.jupiter.api.Test;
+import com.google.gson.Gson;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PostController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+@AutoConfigureRestDocs
 public class PostApiTests {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private Gson gson;
+
+    @MockBean
+    private PostService postService;
+
+    @MockBean
+    private PostMapper postMapper;
+
     @Test
-    public void postPostTest() {
+    public void postPostTest() throws Exception {
         // given
+        PostDto.Post postDto = new PostDto.Post(0, "Post1", "Content1", 1, 0);
+        PostDto.Response responseDto = new PostDto.Response(1, 0, "Post1", "Content1", 1, 0);
+        String content = gson.toJson(postDto);
+
+        given(postMapper.postPostToPost(Mockito.any(PostDto.Post.class))).willReturn(new Post());
+        given(postService.createPost(Mockito.any(Post.class))).willReturn(new Post());
+        given(postMapper.postToPostResponse(Mockito.any(Post.class))).willReturn(responseDto);
 
         //when
+        ResultActions actions = mockMvc.perform(
+                post("/posts")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content)
+        );
 
         //then
+        actions
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.data.postTitle").value(postDto.getPostTitle()))
+                .andExpect(jsonPath("$.data.memberId").value(postDto.getMemberId()))
+                .andExpect(jsonPath("$.data.postContent").value(postDto.getPostContent()))
+                .andDo(document(
+                        "post-post",
+                        ApiDocumentUtils.getRequestPreProcessor(),
+                        ApiDocumentUtils.getResponsePreProcessor(),
+//                        pathParameters(
+//                                parameterWithName();
+//                        )
+                        requestFields(
+                                List.of(
+                                        fieldWithPath("parentId").type(JsonFieldType.NUMBER).description("부모 Id"),
+                                        fieldWithPath("postTitle").type(JsonFieldType.STRING).description("제목"),
+                                        fieldWithPath("postContent").type(JsonFieldType.STRING).description("내용"),
+                                        fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("작성자 Id"),
+                                        fieldWithPath("postView").type(JsonFieldType.NUMBER).description("조회수")
+                                )
+                        ),
+                        responseFields(
+                                List.of(
+                                        fieldWithPath("data").type(JsonFieldType.OBJECT).description("결과 데이터"),
+                                        fieldWithPath("data.id").type(JsonFieldType.NUMBER).description("게시글 Id"),
+                                        fieldWithPath("data.parentId").type(JsonFieldType.NUMBER).description("부모 Id"),
+                                        fieldWithPath("data.postTitle").type(JsonFieldType.STRING).description("제목"),
+                                        fieldWithPath("data.postContent").type(JsonFieldType.STRING).description("내용"),
+                                        fieldWithPath("data.memberId").type(JsonFieldType.NUMBER).description("작성자 Id"),
+                                        fieldWithPath("data.postView").type(JsonFieldType.NUMBER).description("조회수")
+                                )
+                        )
+                ));
     }
 }
---
+++ Server/demo/src/test/java/com/SEB_Pre_020/demo/util/ApiDocumentUtils.java
@@ -0,0 +1,17 @@
+package com.SEB_Pre_020.demo.util;
+
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+
+public interface ApiDocumentUtils {
+    static OperationRequestPreprocessor getRequestPreProcessor() {
+        return preprocessRequest(prettyPrint());
+    }
+
+    static OperationResponsePreprocessor getResponsePreProcessor() {
+        return preprocessResponse(prettyPrint());
+    }
+}
