60ce26d4a542f95b4b5d6ac7565962a8ca1ca111
kjh42447 <kjh42447@naver.com>
2022-10-27 16:09:41 +0900

    edit AnswerService

--- Server/demo/src/main/java/com/SEB_Pre_020/demo/Post/service/AnswerService.java
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/Post/service/AnswerService.java
@@ -7,10 +7,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
+@Service
 public class AnswerService {
     private final PostRepository postRepository;
 
