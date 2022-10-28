b246711430b6e15aefdee74cd045d846d9bcc73d
kjh42447 <kjh42447@gmail.com>
2022-10-28 16:53:30 +0900

    add Vote no testing ver.

--- Server/demo/src/main/java/com/SEB_Pre_020/demo/vote/repository/VoteRepository.java
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/vote/repository/VoteRepository.java
@@ -4,8 +4,10 @@ import com.SEB_Pre_020.demo.vote.entity.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface VoteRepository extends JpaRepository<Vote, Integer> {
-    List<Vote> findByPostId(int postId);
-    List<Vote> findByMemberId(int memberId);
+    List<Vote> findByPost_Id(int postId);
+    List<Vote> findByMember_Id(int memberId);
+    Optional<Vote> findByMember_IdAndPost_Id(int memberId, int postId);
 }
