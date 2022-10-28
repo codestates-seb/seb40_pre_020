623d2cb4c6018eb971f79e5f598063c60968d0b5
SJUANS <lovejuan95@hanmail.net>
2022-10-28 16:40:10 +0900

    feat/Login&Signup: SecurityConfiguration 작성

---
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/config/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.SEB_Pre_020.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .headers().frameOptions().sameOrigin() //동일 출처로부터 들어오는 request만 페이지 렌더링을 허용
+                .and()
+                .csrf().disable()
+                .formLogin()
+                .loginPage("/auths/login-form")
+                .loginProcessingUrl("/process_login")
+                .failureUrl("/auths/login-form?error")
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/")
+                .and()
+                .exceptionHandling().accessDeniedPage("/auths/access-denied")
+                .and()
+                .authorizeHttpRequests(authorize -> authorize
+                        .antMatchers("/profile/memberid/**").hasRole("MEMBER")
+                        // MEMBER 권한 가진 사용자만 마이페이지 이하 리소스 접근가능
+                        .antMatchers("/**").permitAll()
+                );
+        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
