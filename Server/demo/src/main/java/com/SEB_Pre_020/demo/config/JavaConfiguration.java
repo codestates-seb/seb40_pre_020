5b880c9c78f7833c749d66aa1d9cf941ddef03bd
SJUANS <lovejuan95@hanmail.net>
2022-10-28 15:22:37 +0900

    feat/Login: Spring Security 기본 설정 중

---
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/config/JavaConfiguration.java
@@ -0,0 +1,11 @@
+//package com.SEB_Pre_020.demo.config;
+//
+//import com.SEB_Pre_020.demo.member.service.MemberService;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class JavaConfiguration {
+//    @Bean
+//    public MemberService
+//}
