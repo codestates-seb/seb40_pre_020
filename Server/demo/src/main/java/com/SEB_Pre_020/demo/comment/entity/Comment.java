b5169ed3ae2ff02cdd2aea6b30f0ee5e408d4138
kjh42447 <kjh42447@gmail.com>
2022-11-01 00:51:13 +0900

    init Comment

--- Server/demo/src/main/java/com/SEB_Pre_020/demo/comment/entity/Comment.java
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/comment/entity/Comment.java
@@ -1,4 +1,39 @@
 package com.SEB_Pre_020.demo.comment.entity;
 
-public class Comment {
+import com.SEB_Pre_020.demo.audit.Auditable;
+import com.SEB_Pre_020.demo.member.entity.Member;
+import com.SEB_Pre_020.demo.post.entity.Post;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.swing.text.StringContent;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Comment extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="PostId")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "MemberId")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "PostId")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Post post;
+
+    @Column(name = "CommentContent", nullable = false, updatable = true, unique = false)
+    private String commentContent;
 }
--- Server/demo/src/main/java/com/SEB_Pre_020/demo/comment/repository/CommentRepository.java
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/comment/repository/CommentRepository.java
@@ -1,4 +1,10 @@
 package com.SEB_Pre_020.demo.comment.repository;
 
-public interface CommentRepository {
+import com.SEB_Pre_020.demo.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    List<Comment> findByPost_Id(int postId);
 }
