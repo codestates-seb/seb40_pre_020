01cc18d1573110b61914340a602007f39ca0c5e2
kjh42447 <kjh42447@gmail.com>
2022-11-01 13:18:46 +0900

    init CommentController, edit CommentService

--- Server/demo/src/main/java/com/SEB_Pre_020/demo/comment/service/CommentService.java
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/comment/service/CommentService.java
@@ -2,6 +2,8 @@ package com.SEB_Pre_020.demo.comment.service;
 
 import com.SEB_Pre_020.demo.comment.entity.Comment;
 import com.SEB_Pre_020.demo.comment.repository.CommentRepository;
+import com.SEB_Pre_020.demo.post.entity.Post;
+import com.SEB_Pre_020.demo.post.service.PostService;
 import org.springframework.beans.support.PagedListHolder;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
@@ -14,9 +16,11 @@ import java.util.Optional;
 @Service
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final PostService postService;
 
-    public CommentService(CommentRepository commentRepository) {
+    public CommentService(CommentRepository commentRepository, PostService postService) {
         this.commentRepository = commentRepository;
+        this.postService = postService;
     }
 
     /** 댓글 생성 */
@@ -27,6 +31,13 @@ public class CommentService {
 
         if (!commentRepository.existsById(comment.getId())) {
             Comment savedComment = commentRepository.save(comment);
+
+            // 게시글(답글)의 commentCount 증가
+            Post post = postService.findPost(comment.getPost().getId());
+            post.setPostCommentCount(post.getPostCommentCount() + 1);
+            postService.updatePost(post);
+
+
             return savedComment;
         }
 
@@ -68,5 +79,18 @@ public class CommentService {
         Optional<Comment> optionalComment = commentRepository.findById(commentId);
         Comment findComment = optionalComment.orElseThrow();
         return findComment;
+    }
+
+    /** 댓글 삭제 */
+    @Transactional
+    public void deleteComment(int commentId) {
+        Comment findComment = findVerifiedComment(commentId);
+
+        // 게시글(답글)의 commentCount 감소
+        Post post = postService.findPost(findComment.getPost().getId());
+        post.setPostCommentCount(post.getPostCommentCount() - 1);
+        postService.updatePost(post);
+
+        commentRepository.delete(findComment);
     }
 }
