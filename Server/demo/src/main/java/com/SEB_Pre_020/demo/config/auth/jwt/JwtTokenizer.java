d06e36c22e5260ac45fee1a0888fecb575629e8c
SJUANS <lovejuan95@hanmail.net>
2022-11-02 12:06:19 +0900

    SecurityConfiguration: 회원용(Role=MEMBER)접근 권한 설정 / jwtTokenizer 작성중

--- Server/demo/src/main/java/com/SEB_Pre_020/demo/config/auth/jwt/JwtTokenizer.java
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/config/auth/jwt/JwtTokenizer.java
@@ -1,17 +1,31 @@
-package com.SEB_Pre_020.demo.config.auth;
+package com.SEB_Pre_020.demo.config.auth.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.Value;
 
+import javax.annotation.PostConstruct;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Base64;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
 
 public class JwtTokenizer {
+
+    private String secretKey = "iv1idjidIlgi1hii1dijhciIj1!ih|giI";
+    // 코드에서 환경변수로 빼내 숨기는 방법 적용해야 함
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }   // 객체 초기화, secretKey를 Base64로 인코딩한다.
+
     public String encodeBase64SecretKey(String secretKey) {
         return Encoders.BASE64.encode(secretKey.getBytes(StandardCharsets.UTF_8));
     }
@@ -22,6 +36,7 @@ public class JwtTokenizer {
                                       String base64EncodedSecretKey) {
         Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
         //  Base64 형식 Secret Key 문자열을 이용해 Key(java.security.Key) 객체 얻기
+
 
         return Jwts.builder()
                 .setClaims(claims) // 인증된 사용자와 관련된 정보 추가
@@ -50,5 +65,25 @@ public class JwtTokenizer {
         // key byte array를 기반으로 HMAC 알고리즘을 적용한 Key(java.security.Key) 객체를 생성
 
         return key;
+    } // JWT 서명에 사용하는 SecretKey 생성 메서드
+
+    public void verifySignature(String jws, String base64EncodedSecretKey) {
+        Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
+
+        Jwts.parserBuilder()
+                .setSigningKey(key)     // (1)
+                .build()
+                .parseClaimsJws(jws);   // (2)
     }
-} // JWT 서명에 사용하는 SecretKey 생성 메서드
+
+    // 토큰의 유효성 + 만료일자 확인
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
