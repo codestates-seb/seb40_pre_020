7d6d04626a13657055f86369a2fc4b158de35470
SJUANS <lovejuan95@hanmail.net>
2022-11-04 10:45:05 +0900

    JwtAuthenticationFilter 추가

--- Server/demo/src/main/java/com/SEB_Pre_020/demo/config/SecurityConfiguration.java
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/config/SecurityConfiguration.java
@@ -1,30 +1,36 @@
 package com.SEB_Pre_020.demo.config;
 
 import com.SEB_Pre_020.demo.config.auth.filter.JwtAuthenticationFilter;
+import com.SEB_Pre_020.demo.config.auth.filter.JwtVerificationFilter;
 import com.SEB_Pre_020.demo.config.auth.jwt.JwtTokenizer;
+import com.SEB_Pre_020.demo.config.auth.utils.CustomAuthorityUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 public class SecurityConfiguration {
     private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils authorityUtils;
 
-    public SecurityConfiguration(JwtTokenizer jwtTokenizer) {
+    public SecurityConfiguration(JwtTokenizer jwtTokenizer, CustomAuthorityUtils authorityUtils) {
         this.jwtTokenizer = jwtTokenizer;
+        this.authorityUtils = authorityUtils;
     }
 
     @Bean
@@ -32,7 +38,9 @@ public class SecurityConfiguration {
         http
                 .headers().frameOptions().sameOrigin() //동일 출처로부터 들어오는 request만 페이지 렌더링을 허용
                 .and()
-                .csrf().requireCsrfProtectionMatcher(new AntPathRequestMatcher("!/h2/**"))
+                .csrf().disable()
+                .cors(withDefaults())
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .headers().addHeaderWriter
                         (new StaticHeadersWriter("X-Content-Security-Policy", "script-src 'self'"))
@@ -40,6 +48,12 @@ public class SecurityConfiguration {
                 .and()
                 .formLogin().disable()
                 .httpBasic().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(new MemberAuthenticationEntryPoint())
+                .accessDeniedHandler(new MemberAccessDeniedHandler())
+                .and()
+                .apply(new CustomFilterConfigurer())
+                .and()
                 .authorizeHttpRequests(authorize -> authorize
                         .antMatchers("/profiles/**").hasRole("MEMBER")
                         // MEMBER 권한 가진 사용자만 마이페이지 이하 리소스 접근가능
@@ -70,8 +84,14 @@ public class SecurityConfiguration {
             JwtAuthenticationFilter jwtAuthenticationFilter
                     = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
             jwtAuthenticationFilter.setFilterProcessesUrl("/v1/auth/login"); // Login url
+            jwtAuthenticationFilter.setAuthenticationSuccessHandler(new MemberAuthenticationSuccessHandler());
+            jwtAuthenticationFilter.setAuthenticationFailureHandler(new MemberAuthenticationFailureHandler());
 
-            builder.addFilter(jwtAuthenticationFilter);
+            JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer, authorityUtils);
+
+            builder
+                    .addFilter(jwtAuthenticationFilter)
+                    .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class);
         }
     } // JwtAuthenticationFilter를 등록하는 메서드
 
