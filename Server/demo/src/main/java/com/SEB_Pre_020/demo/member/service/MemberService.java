aa3df837a618b3aa8d35e5615d93b59798f428ca
SJUANS <lovejuan95@hanmail.net>
2022-11-05 01:38:50 +0900

    memberControllerApiTests: 회원가입 API 테스팅 코드(+문서화) 작성중

--- Server/demo/src/main/java/com/SEB_Pre_020/demo/member/service/MemberService.java
+++ Server/demo/src/main/java/com/SEB_Pre_020/demo/member/service/MemberService.java
@@ -8,7 +8,6 @@ import com.SEB_Pre_020.demo.exception.BusinessLogicException;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -76,8 +75,9 @@ public class MemberService {
 
 //    public Member getUserByToken(){
 //        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        PrincipalDetails principalDetails = (PrincipalDetails)principal;
+////        PrincipalDetails principalDetails = (PrincipalDetails)principal;
 //
-//        return principalDetails.getMember();
+//
+//        return .getMember();
 //    }
 }
